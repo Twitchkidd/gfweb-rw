260f6406d8e02cae62308b60d7b58685c776b6d7
Gareth Field <field.gareth@gmail.com>
2021-03-03 08:47:47 -0500

    first page!

---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,14 @@
+import { Link, routes } from '@redwoodjs/router';
+
+const HomePage = () => {
+  return (
+    <>
+      <h1>Gareth DOT WEB!!!</h1>
+      <p>
+        Leh go.
+      </p>
+    </>
+  );
+};
+
+export default HomePage;
